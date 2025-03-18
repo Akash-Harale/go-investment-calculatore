7a5a676a81f32ac95740f0660c62b2427283dc87
Akash <akash.v.harale@gmail.com>
2025-03-18 10:04:48 +0530

    declaration of const variable

--- investment_calculator.go
+++ investment_calculator.go
@@ -10,6 +10,8 @@ func main() {
 	 expectedReturnRate := 5.5
 	var years float64 = 10
 
+	const inflation float64= 5.5
+
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	fmt.Println(futureValue)
