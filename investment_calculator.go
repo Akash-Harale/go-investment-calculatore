58376cb9f08153cb545daa8e93c2dcd6cdc8872f
Akash <akash.v.harale@gmail.com>
2025-03-18 10:39:50 +0530

    scan method in fmt package to take input from user

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,13 +6,20 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	 expectedReturnRate := 5.5
+	var investmentAmount float64
+	expectedReturnRate := 5.5
 	var years float64 = 10
+	const inflationRate = 2.5
+	fmt.Print("investment Amout: ")
+	fmt.Scan(&investmentAmount)
 
-	const inflation float64= 5.5
+	// const values can't be changes later and it should be initialized at the time of decalration.
+	// variables declared with var keyward can be decalred first and initilized later.
+	
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
